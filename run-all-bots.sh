bda52fe83713eb0aab98a81d62a2818d86da6a22
viola he <viola@nyu.edu>
2025-12-19 16:27:50 -0800

    more bots yaml

--- run-all-bots.sh
+++ run-all-bots.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-# Run all bots simultaneously
+# Run all bots simultaneously (strangeopus45 + claude0-4)
 
 cd "$(dirname "$0")"
 
 echo "Starting all bots..."
 
-# Bot 1: Default token (discord_token)
-DISCORD_TOKEN_FILE=./discord_token CACHE_PATH=./cache1 npm run dev &
-PID1=$!
-echo "Started bot 1 (PID: $PID1)"
+# strangeopus45 (original bot)
+DISCORD_TOKEN_FILE=./discord_token CACHE_PATH=./cache_opus npm run dev &
+echo "Started strangeopus45 (PID: $!)"
 
-# Bot 2: claude3sonnet (discord_token_2)
+# claude0
+DISCORD_TOKEN_FILE=./discord_token_0 CACHE_PATH=./cache0 npm run dev &
+echo "Started claude0 (PID: $!)"
+
+# claude1
+DISCORD_TOKEN_FILE=./discord_token_1 CACHE_PATH=./cache1 npm run dev &
+echo "Started claude1 (PID: $!)"
+
+# claude2
 DISCORD_TOKEN_FILE=./discord_token_2 CACHE_PATH=./cache2 npm run dev &
-PID2=$!
-echo "Started bot 2 (PID: $PID2)"
+echo "Started claude2 (PID: $!)"
+
+# claude3
+DISCORD_TOKEN_FILE=./discord_token_3 CACHE_PATH=./cache3 npm run dev &
+echo "Started claude3 (PID: $!)"
+
+# claude4
+DISCORD_TOKEN_FILE=./discord_token_4 CACHE_PATH=./cache4 npm run dev &
+echo "Started claude4 (PID: $!)"
 
 echo ""
-echo "All bots started. Press Ctrl+C to stop all."
+echo "All 6 bots started. Press Ctrl+C to stop all."
 
-# Wait for any to exit
 wait
